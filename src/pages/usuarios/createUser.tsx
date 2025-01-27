869526012fe93601b48dc1adcc1d02d9e2084493
Thiago Campos <thiagoabcfc@hotmail.com>
2025-01-26 21:46:13 -0300

    feat: associa museu na tela de criar usuario

--- src/pages/usuarios/createUser.tsx
+++ src/pages/usuarios/createUser.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react"
+import React, { useEffect, useState, useCallback } from "react"
 import { useNavigate } from "react-router-dom"
 import DefaultLayout from "../../layouts/default"
-import { useMutation, useSuspenseQuery } from "@tanstack/react-query" //add useSuspenseQuery
+import { useMutation, useSuspenseQuery, useQuery } from "@tanstack/react-query"
 import Input from "../../components/Input"
-import { Select } from "react-dsgov"
+import { Select, Row, Col, Button } from "react-dsgov"
 import { z } from "zod"
-import { useForm, Controller } from "react-hook-form" //add Controller
+import { useForm, Controller } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import clsx from "clsx"
 import { Link } from "react-router-dom"
 import request from "../../utils/request"
 import toast from "react-hot-toast"
+import { debounce } from "lodash"
 
 const schema = z
   .object({
@@ -19,7 +20,8 @@ const schema = z
     profile: z.string().min(1, "Este campo é obrigatório"),
     password: z.string().min(1, "Este campo é obrigatório"),
     confirmPassword: z.string().min(1, "Este campo é obrigatório"),
-    especialidadeAnalista: z.array(z.string()).optional()
+    especialidadeAnalista: z.array(z.string()).optional(),
+    museus: z.array(z.string()).optional()
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "As senhas não são iguais",
@@ -33,8 +35,76 @@ interface Profile {
   description: string
 }
 
+interface Museu {
+  _id: string
+  nome: string
+}
+
+interface Paginacao {
+  currentPage: number
+  totalPages: number
+  totalItems: number
+  itemsPerPage: number
+}
+
+interface RespostaMuseus {
+  museus: Museu[]
+  pagination: Paginacao
+}
+
+const fetchMuseus = async (
+  search: string,
+  page: number
+): Promise<RespostaMuseus> => {
+  const response = await request(
+    `/api/admin/museus?semVinculoUsuario=true&search=${search}&page=${page}`
+  )
+  if (!response.ok) throw new Error("Erro ao carregar museus")
+
+  const data = await response.json()
+
+  return {
+    museus: data.museus || [],
+    pagination: data.pagination || {
+      currentPage: 0,
+      totalPages: 0,
+      totalItems: 0,
+      itemsPerPage: 0
+    }
+  }
+}
+
 const CreateUser: React.FC = () => {
   const [isAnalyst, setIsAnalyst] = useState(false)
+  const [isDeclarant, setIsDeclarant] = useState(false)
+  const [selectedMuseus, setSelectedMuseus] = useState<string[]>([])
+  const [selectedMuseusNames, setSelectedMuseusNames] = useState<string[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [search, setSearch] = useState("")
+  const [page, setPage] = useState(1)
+
+  const { data: museusData } = useQuery<RespostaMuseus>({
+    queryKey: ["museus", search, page],
+    queryFn: () => fetchMuseus(search, page),
+    enabled: search.length > 0
+  })
+
+  const museus = museusData?.museus || []
+
+  const debounceSearch = useCallback(
+    debounce((value: string) => {
+      setIsLoading(true)
+      setSearch(value)
+      setPage(1)
+    }, 500),
+    [setIsLoading, setSearch, setPage]
+  )
+
+  useEffect(() => {
+    if (museus.length > 0) {
+      setIsLoading(false)
+    }
+  }, [museus])
 
   const { data: profiles } = useSuspenseQuery<Profile[]>({
     queryKey: ["profiles"],
@@ -46,6 +116,7 @@ const CreateUser: React.FC = () => {
       return response.json()
     }
   })
+
   const {
     register,
     handleSubmit,
@@ -65,23 +136,28 @@ const CreateUser: React.FC = () => {
       nome,
       password,
       profile,
-      especialidadeAnalista
-    }: FormData) => {
-      //add profile
-
+      especialidadeAnalista,
+      museus
+    }: FormData & { museus: string[] }) => {
       const res = await request("/api/admin/users", {
         method: "POST",
-        data: {
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
           email,
           nome,
           profile,
           senha: password,
-          especialidadeAnalista: isAnalyst ? especialidadeAnalista : []
-          //museus: []
-        }
+          especialidadeAnalista: isAnalyst ? especialidadeAnalista : [],
+          museus: isDeclarant ? museus : []
+        })
       })
 
-      return await res.json()
+      if (!res.ok) {
+        const errorData = await res.json()
+        throw new Error(errorData.message || "Erro ao criar usuário")
+      }
+
+      return res.json()
     },
     onSuccess: () => {
       navigate("/usuarios")
@@ -100,7 +176,9 @@ const CreateUser: React.FC = () => {
     confirmPassword,
     especialidadeAnalista
   }: FormData) => {
-    //add profile
+    const museusIds = isDeclarant
+      ? selectedMuseus.map((item) => item.split(",")[0])
+      : []
 
     mutate({
       email,
@@ -108,8 +186,9 @@ const CreateUser: React.FC = () => {
       password,
       profile,
       confirmPassword,
-      especialidadeAnalista
-    }) //add profile
+      especialidadeAnalista,
+      museus: museusIds
+    })
   }
 
   const profileOptions = profiles.map((profile) => {
@@ -125,11 +204,11 @@ const CreateUser: React.FC = () => {
     }
   })
 
-  // Monitorar mudanças no campo 'profile' para habilitar/desabilitar o multiselect
   const selectedProfile = watch("profile")
-  React.useEffect(() => {
+  useEffect(() => {
     const selected = profiles.find((p) => p._id === selectedProfile)
     setIsAnalyst(selected?.name.toLowerCase() === "analyst")
+    setIsDeclarant(selected?.name.toLowerCase() === "declarant")
   }, [selectedProfile, profiles])
 
   return (
@@ -172,7 +251,7 @@ const CreateUser: React.FC = () => {
             <legend className="text-lg font-extrabold px-3 m-0">
               Controle de acesso
             </legend>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-2">
               <Controller
                 name="profile"
                 control={control}
@@ -208,6 +287,94 @@ const CreateUser: React.FC = () => {
                   )}
                 />
               )}
+              {isDeclarant && (
+                <div>
+                  <Row>
+                    <Col>
+                      <Controller
+                        control={control}
+                        name="museus"
+                        render={({ field }) => (
+                          <>
+                            <Select
+                              type="multiple"
+                              selectAllText={""}
+                              label="Museus"
+                              placeholder="Digite para buscar..."
+                              options={
+                                museus.length > 0
+                                  ? museus.map((m: Museu) => ({
+                                      label: m.nome,
+                                      value: `${m._id},${m.nome}`
+                                    }))
+                                  : []
+                              }
+                              className="!w-full"
+                              {...field}
+                              value={selectedMuseus}
+                              onInput={(e) => {
+                                const inputValue = (
+                                  e.target as HTMLInputElement
+                                ).value
+                                debounceSearch(inputValue)
+                              }}
+                              onChange={(selected: string[]) => {
+                                field.onChange(selected)
+
+                                const nomesMuseus = selected.map(
+                                  (item) => item.split(",")[1]
+                                )
+                                setSelectedMuseus(selected)
+                                setSelectedMuseusNames(nomesMuseus)
+                              }}
+                            />
+                            {isLoading && (
+                              <p className="text-sm text-gray-500 mt-2">
+                                Carregando museus...
+                              </p>
+                            )}
+                          </>
+                        )}
+                      />
+                    </Col>
+                  </Row>
+                </div>
+              )}
+              <div className="mt-4 w-full grid">
+                {isDeclarant && selectedMuseusNames.length > 0 && (
+                  <>
+                    <p className="text-sm text-gray-500 mb-2">
+                      {selectedMuseusNames.length} museu(s) selecionado(s):
+                    </p>
+                    <div className="flex flex-wrap gap-2 p-2">
+                      {selectedMuseusNames.map((name, index) => (
+                        <Button
+                          key={index}
+                          className="gap-2 flex items-center justify-between"
+                          primary
+                          inverted
+                        >
+                          <i
+                            className="fa-solid fa-xmark ml-2 cursor-pointer"
+                            onClick={() => {
+                              const updatedMuseus = selectedMuseus.filter(
+                                (_, i) => i !== index
+                              )
+                              const updatedNames = selectedMuseusNames.filter(
+                                (_, i) => i !== index
+                              )
+
+                              setSelectedMuseus(updatedMuseus)
+                              setSelectedMuseusNames(updatedNames)
+                            }}
+                          ></i>
+                          {name}
+                        </Button>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <Input
@@ -226,7 +393,6 @@ const CreateUser: React.FC = () => {
               />
             </div>
           </fieldset>
-          {/* Botões */}
           <div className="flex justify-end space-x-4">
             <Link to="/usuarios" className="br-button secondary">
               Voltar
